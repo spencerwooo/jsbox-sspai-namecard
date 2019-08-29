372f60dbb225f9a843b29c5ad8064b75f63e837b
Spencer Woo <32114380+spencerwooo@users.noreply.github.com>
2019-08-29 12:25:58 +0800

    update: username

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ var requests = require('scripts/requests')
 // var sspaiUserName = 'SpencerWoo'
 
 // 请在这里将 userId 的值替换为你的少数派用户 ID，在你的少数派用户主页链接中即可找到
-// 比如：https://beta.sspai.com/user/ardazhlq/updates 中, ardazhlq 即为少数派 userId
-var userId = 'ardazhlq'
+// 比如：https://beta.sspai.com/user/spencerwoo/updates 中, spencerwoo 即为少数派 userId
+var userId = 'spencerwoo'
 
 function main() {
   // 通过搜索接口获取少数派用户 ID，目前访问速度非常慢，以后再说
